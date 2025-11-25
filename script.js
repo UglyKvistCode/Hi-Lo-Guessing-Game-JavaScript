0e921ceb1196fe51273022ccfa0a37d7fdeac98f
Maria Kvist <mariakvist85@gmail.com>
2025-11-25 14:13:40 +0100

    game is finished

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-var number = Math.round(Math.random() * 100 + 1);
-var guess = prompt("Guess a number between 1 and 100:")
+let number = Math.round(Math.random() * 100 + 1);
+let guess = "";
+while (guess != number) {
+    guess = prompt("Guess a number between 1 and 100:");
+if ( guess > number )
+    alert("Too high.");
+else if ( guess < number )
+    alert("Too low.");
+else 
+    alert("Correct!")
+
+}
